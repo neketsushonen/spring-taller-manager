43aa1045925a9f08a04ba4ee986e91bee3ce7bfd
Chun Hau Lai <c.lai@adevcom.cl>
2018-08-05 23:15:47 -0400

    Agregar el ejercicio 01

---
+++ src/main/java/cl/lai/manager/taller/ejercicio/App_ejercicio_01.java
@@ -0,0 +1,24 @@
+package cl.lai.manager.taller.ejercicio;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cl.lai.manager.taller.service.IVendedorService;
+import cl.lai.manager.taller.service.VendedorService;
+
+/**
+ * @author chunhaulai
+ *
+ */
+public class App_ejercicio_01 {
+    public static void main( String[] args ){
+    	//leer el archivo del contexto
+    	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springApplicationContext.xml");
+    	
+    	//Obtener el objeto JdbcTemplate y realizar la insercion del vendedor
+    	
+    	//Obtener el objeto JdbcTemplate y realizar la consulta del vendedor
+    	
+    	
+    }
+}
