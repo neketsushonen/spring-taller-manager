5700ecf4363d1e9c2e82937375e1e921b3996675
Chun Hau Lai <c.lai@adevcom.cl>
2018-09-11 12:26:17 -0300

    Incorporacion de Eureka Client

---
+++ src/main/java/cl/lai/manager/taller/App_tutorial_15.java
@@ -0,0 +1,25 @@
+package cl.lai.manager.taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+
+/**
+ * @author chunhaulai
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = { DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class App_tutorial_15 {
+    public static void main( String[] args ){
+    	//leer el archivo del contexto
+		ApplicationContext applicationContext = SpringApplication.run(App_tutorial_15.class, args);
+		 
+     	//Abrir el browser e introduzca el siguiente link: http://localhost:8080
+      
+    }
+}
