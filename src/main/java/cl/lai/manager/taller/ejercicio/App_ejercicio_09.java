ab2d7531a64f9e1bc7d8c2e8551796c32d8b8873
chun hau lai <c.lai@adevcom.cl>
2021-04-07 15:43:12 -0400

    refactor: agregar definición del problema

--- src/main/java/cl/lai/manager/taller/ejercicio/App_ejercicio_09.java
+++ src/main/java/cl/lai/manager/taller/ejercicio/App_ejercicio_09.java
@@ -9,9 +9,10 @@ public class App_ejercicio_09 {
     public static void main( String[] args ){
     	//1. Implementar el metodo actualizar en cl.lai.manager.taller.controller.MiTallerController que recibe tres parametros: id, codigo y descripcion 
     	//   cuyo link sea /mitaller/categoria con metodo PUT
-    	//2. Implementar el metodo eliminar en cl.lai.manager.taller.controller.MiTallerController que recibe 1 parametro: id, codigo y descripcion 
+    	//2. Implementar el metodo eliminar en cl.lai.manager.taller.controller.MiTallerController que recibe 1 parametro: id 
     	//    cuyo link sea /mitaller/categoria con metodo DELETE
     	//3. Instanciar la aplicacion App_ejercicio_09 con Spring boot
     	//4. Abrir el postman e introduzca http://localhost:8080/mitaller/categoria y prueba los métodos PUT y DELETE con los problemas correspondientes
+		//5. Investigar el uso de @PathVariable y aplicarlo en la ruta del DELETE del punto 3
     }
 }
