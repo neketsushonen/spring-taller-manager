3202be694acbe95bec235c8925de252c933cf55c
chun hau lai <c.lai@adevcom.cl>
2021-04-07 09:27:30 -0400

    doc: corrección de definición del problema

--- src/main/java/cl/lai/manager/taller/ejercicio/App_ejercicio_08.java
+++ src/main/java/cl/lai/manager/taller/ejercicio/App_ejercicio_08.java
@@ -9,9 +9,9 @@ public class App_ejercicio_08 {
     public static void main( String[] args ){
     	//1. Implementar el archivo llamado mitaller.html en src/main/resources/templates que imprima la variable ${mitaller}
     	//2. Implementar el controller cl.lai.manager.taller.controller.MiTallerController 
-    	//3. Implementar el metodo dosVariable que recibe dos parametros desde URL: param1 y param2
+    	//3. Implementar el metodo GET /mitaller que recibe dos parametros desde URL: param1 y param2
     	//4. Definir el atributo "mitaller" que contiene la concatenacion del valor de los parametros param1 y param2
-    	//5. Retornar a la pagina "mitaller" 
+    	//5. Retornar a la pagina "mitaller" desde el método del Controller
     	//6. Instanciar la aplicacion App_ejercicio_08 con Spring boot
     	//7. Abrir el browser e introduzca http://localhost:8080/taller?param1=mi&param2=taller
     }
